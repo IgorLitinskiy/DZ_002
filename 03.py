c8a4cc5b045c94492e756a0e9b30a6926ff9f439
IgorLitinskiy <113501865+IgorLitinskiy@users.noreply.github.com>
2022-12-03 16:54:33 +0300

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,11 @@
+#Реализуйте алгоритм перемешивания списка. Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
+import random
+print ("Начальный список: ")
+list = [0,1,2,3,4,5,6,7,8,9]
+print (list)
+list = [0,1,2,3,4,5,6,7,8,9]
+for i in range(len(list)-1, 0, -1):
+    j = random.randint(0, i + 1) 
+    list[i], list[j] = list[j], list[i] 
+print ("Перемешанный список: ")
+print(list)
